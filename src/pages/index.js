b0dd5807172ee81764f082fc8319c7875d058afb
Jeremy Kepf <jeremy1975@gmail.com>
2020-04-06 16:29:58 -0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,19 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Just a basic site built for quick access to soccer tutorial videos.</p>
+    <p>I'll continue to improve and add to this site as I have time, but I wanted to get something up as quickly as possible.</p>
+    <p>Thank you Ashley, and thank you Kevin for putting them together.</p>
+    <p>Click on your team below to access their tutorial videos.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div>
+      <Link to="/page-2/">2010 Girls - Coach Kevin</Link>
+    </div>
+    <div>
+      <Link to="/page-3/">2012 Girls - Coach Ashley</Link>
+    </div>
   </Layout>
 )
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,12 +4,26 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import YouTube from "react-youtube"
+
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>2010 Girls</h1>
+    <p>Click on each embedded video to play.</p>
     <Link to="/">Go back to the homepage</Link>
+    <section>
+      <h3>Remote Training Session 1</h3>
+      <YouTube videoId="klj15Nrtak4"></YouTube>
+    </section>
+    <section>
+      <h3>Remote Training Session 2</h3>
+      <YouTube videoId="cvBA9rL4dVQ"></YouTube>
+    </section>
+    <section>
+      <h3>Remote Training Session 3</h3>
+      <YouTube videoId="sYgOeQd5xyI"></YouTube>
+    </section>
   </Layout>
 )
 
